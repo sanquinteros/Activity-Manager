db70b259d7ef1da9cfa7cbc25a98df8d0931e9da
quinteros-santiago <santiagoqros@gmail.com>
2022-11-08 18:52:27 -0300

    Changed variable message to static.

--- src/views/WelcomeView.c
+++ src/views/WelcomeView.c
@@ -2,9 +2,9 @@ void showWelcomeView() {
 	int selectedView;
     
 	printf("You are in the \"WELCOME\" window.\n");
-	printf("Enter [%i] if you want to close the software.\n", CLOSE_VIEW);
-	printf("Enter [%i] if you want to login.\n", LOGIN_VIEW);
-	printf("Enter [%i] if you want to register.\n", REGISTER_VIEW);
+	printf("Enter [1] if you want to log in.\n");
+	printf("Enter [2] if you want to register.\n");
+	printf("Enter [3] if you want to close the software.\n");
     printf("Enter here: ");
 	scanf("%i", &selectedView);
 
