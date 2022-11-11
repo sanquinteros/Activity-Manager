a6451e3d1d42aee3388e7cbbf356c06748fdb491
quinteros-santiago <santiagoqros@gmail.com>
2022-11-11 17:25:28 -0300

    Moved register process from view to service.

--- src/services/RegisterService.c
+++ src/services/RegisterService.c
@@ -45,3 +45,40 @@ Register registerServiceValidateRole(Register rregister) {
     }
     return rregister;
 }
+
+Register getRegisterFromUser() {
+    Register rregister;
+
+    printf("Enter your name: ");
+    scanf("%s", rregister.username);
+    rregister = registerServiceValidateUsername(rregister);
+	printf("Enter your password: ");
+	scanf("%s", rregister.password);
+	rregister = registerServiceValidatePassword(rregister);
+    printf("If you want to register as an \"Administrator\", enter [%i].\n", ADMIN_ROLE);
+    printf("If you want to register as a \"Client\", enter [%i].\n", CLIENT_ROLE);
+    printf("If you want to register as a \"Worker\", enter [%i].\n", WORKER_ROLE);
+    printf("Enter your role: ");
+    scanf("%i", &rregister.role);
+	getchar();
+
+    rregister = registerServiceValidateRole(rregister);
+
+    return rregister;
+}
+
+void registerServiceRegisterUser() {
+    int userWasCreated;
+    Register rregister;
+    User userFromRegister;
+
+    rregister = getRegisterFromUser();
+    userFromRegister = constructUserFromRegister(rregister);
+    userWasCreated = userRepositoryCreateUser(userFromRegister);
+
+    if (userWasCreated == 1) {
+        printf("You have successfully been registered.\n");
+    } else {
+        printf("An unexpected error occour, your user was not created.\n");
+    }
+}
--- src/views/RegisterView.c
+++ src/views/RegisterView.c
@@ -1,33 +1,7 @@
-Register getRegister() {
-    Register rregister;
-
-    printf("Enter your name: ");
-    scanf("%s", rregister.username);
-    rregister = registerServiceValidateUsername(rregister);
-	printf("Enter your password: ");
-	scanf("%s", rregister.password);
-	rregister = registerServiceValidatePassword(rregister);
-    printf("If you want to register as an \"Administrator\", enter [%i].\n", ADMIN_ROLE);
-    printf("If you want to register as a \"Client\", enter [%i].\n", CLIENT_ROLE);
-    printf("If you want to register as a \"Worker\", enter [%i].\n", WORKER_ROLE);
-    printf("Enter your role: ");
-    scanf("%i", &rregister.role);
-	getchar();
-
-    rregister = registerServiceValidateRole(rregister);
-
-    return rregister;
-}
-
 void showRegisterView() {
 	printf("You are in the \"REGISTER\" window.\n");
 
-    int userWasCreated = userRepositoryCreateUser(constructUserFromRegister(getRegister()));
+    registerServiceRegisterUser();
 
-    if (userWasCreated == 1) {
-        printf("You have successfully been registered.\n");
-    } else {
-        printf("An unexpected error occour, your user was not created.\n");
-    }
     redirectTo(WELCOME_VIEW);
 }
