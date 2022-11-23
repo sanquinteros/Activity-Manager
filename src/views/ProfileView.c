909f1ad8abb84ae6eb93c4daf1e9471701d52052
quinteros-santiago <santiagoqros@gmail.com>
2022-11-23 01:47:40 -0300

    Removed updating name feature from profile window.

--- src/views/ProfileView.c
+++ src/views/ProfileView.c
@@ -1,25 +1,23 @@
 void showProfileView() {
 	printf("You are in the \"PROFILE\" window.\n\n");
-	printf("You are a \"%s\", your name is \"%s\", your password is \"%s\".\n\n", getRoleName(currentUser.role), currentUser.username, currentUser.password);
-	printf("Enter [1] if you want to update your name (you can not).\n");
-	printf("Enter [2] if you want to update your password.\n");
-	printf("Enter [3] if you want to go back to the \"MAIN\" window.\n");
+
+	printf("You are %s, your name is \"%s\", your password is \"%s\".\n\n", getRoleName(currentUser.role), currentUser.username, currentUser.password);
+/*
+    The system has to consider various overlooked details in order to update the name of the user,
+    we can't support this feature right now.
+
+	printf("Enter [] if you want to update your name.\n");
+*/
+	printf("Enter [1] if you want to update your password.\n");
+	printf("Enter [2] if you want to go back to the \"MAIN\" window.\n");
     printf("Enter here: ");
 	scanf("%i", &selectedView);
 	getchar();
 
     if (selectedView == 1) {
-/*
-        In order to update names, I have to update them on ClientRequest table too.
-        It could come on a future release, but currently I dont have any plans on doing so.
-
-        function name -> show(UPDATENAME_VIEW);
-*/
-        redirectTo(PROFILE_VIEW);
-    } else if (selectedView == 2) {
         show(UPDATEPASSWORD_VIEW);
         redirectTo(PROFILE_VIEW);
-    } else if (selectedView == 3) {
+    } else if (selectedView == 2) {
         show(MAIN_VIEW);
     } else {
         popInvalidViewMessage();
